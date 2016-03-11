3520a06584e665f08371b6330d7dac2f8e15ac0c
WuIFan <denny91002@gmail.com>
2016-03-11 22:31:28 +0800

    Minecraft的HelloWorld

---
+++ HelloBu/src/denny/bukkit/helloworld/main.java
@@ -0,0 +1,13 @@
+package denny.bukkit.helloworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+	public void onEnable(){
+		//getLogger().info("¥¿¦b¶}±Ò....");
+		getLogger().info("¶Ù¶Ù¶Ù¶Ù¶Ù¶Ù");
+	}
+	public void onDisable(){
+		getLogger().info("ÙT¹Æ");
+	}
+}
